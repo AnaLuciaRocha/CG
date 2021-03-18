5aaf24b4467fafa0b9704e1f254d42999cae06e7
Ana Rocha <46908402+AnaLuciaRocha@users.noreply.github.com>
2021-03-18 09:28:33 +0000

    P1 (#1)
    * tp1.ex1 folder created and readme file updated
    
    * ex1 works, missing report
    
    * readme updated
    
    * readme
    
    * readme finished and some improvements where made

---
+++ build/external/glfw-3.3.3/tests/CMakeFiles/timeout.dir/DependInfo.cmake
@@ -0,0 +1,33 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/ana/Documents/CG/external/glfw-3.3.3/deps/glad_gl.c" "/home/ana/Documents/CG/build/external/glfw-3.3.3/tests/CMakeFiles/timeout.dir/__/deps/glad_gl.c.o"
+  "/home/ana/Documents/CG/external/glfw-3.3.3/tests/timeout.c" "/home/ana/Documents/CG/build/external/glfw-3.3.3/tests/CMakeFiles/timeout.dir/timeout.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "GLEW_STATIC"
+  "TW_NO_DIRECT3D"
+  "TW_NO_LIB_PRAGMA"
+  "TW_STATIC"
+  "_CRT_SECURE_NO_WARNINGS"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../external/glfw-3.3.3/deps"
+  "../external/glfw-3.3.3/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/ana/Documents/CG/build/external/glfw-3.3.3/src/CMakeFiles/glfw.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ tp1.ex1/main.cpp
@@ -0,0 +1,129 @@
+//1. make
+//2. ./tp1.ex1/tp1.ex1
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+// settings
+const unsigned int SCR_WIDTH = 400;
+const unsigned int SCR_HEIGHT = 400;
+const float MAX_TIME_WINDOW = 30.0;
+
+
+//Change backgroung color when keys R,G and B are pressed
+void changeBackgoundColor(GLFWwindow *window)
+{
+  
+  if (glfwGetKey(window, GLFW_KEY_R) == GLFW_PRESS)
+  {
+    glClearColor(1, 0, 0, 0); // Red
+  }
+  else if (glfwGetKey(window, GLFW_KEY_G) == GLFW_PRESS)
+  {
+    glClearColor(0, 1, 0, 0); // Green
+  }
+  else if (glfwGetKey(window, GLFW_KEY_B) == GLFW_PRESS)
+  {
+    glClearColor(0, 0, 1, 0); // Blue
+  }
+  glClear(GL_COLOR_BUFFER_BIT);
+
+}
+
+/* process all input: query GLFW whether relevant keys are pressed/released 
+this frame and react accordingly 
+-----------------------------------------------------------------------*/
+// Funcao que deteta o Enter
+void processInput(GLFWwindow *window)
+{
+  if (glfwGetKey(window, GLFW_KEY_ENTER) == GLFW_PRESS)
+    glfwSetWindowShouldClose(window, true);
+  changeBackgoundColor(window);
+}
+
+/* glfw: whenever the window size changed (by OS or user resize) this
+   callback function executes
+   -------------------------------------------------------------------*/
+// Funcao que cria a janela e deteta movimento, dimensoes,etc
+void framebuffer_size_callback(GLFWwindow *window, int width, int height)
+{
+  // make sure the viewport matches the new window dimensions; note that
+  // width and height will be significantly larger than specified on
+  // retina displays.
+  glViewport(0, 0, width, height);
+}
+
+
+
+void renderLoop(GLFWwindow *window)
+{
+    while (!glfwWindowShouldClose(window))
+  {
+    
+    // input
+    // -----
+    processInput(window);
+
+    /* glfw: swap buffers and poll IO events (keys pressed/released, 
+	 mouse moved etc.)
+	 --------------------------------------------------------------*/
+    //cria janela e processa tudo o que nela se passa
+    glfwSwapBuffers(window);
+    //changeBackgoundColor(window);
+    
+    glfwPollEvents();
+    
+    //std::cout << (glfwGetTime());
+    float time_ellapsed = glfwGetTime();
+    if(time_ellapsed >= MAX_TIME_WINDOW){
+      glfwSetWindowShouldClose(window, true);
+    }
+  }
+}
+
+int main()
+{
+
+  // glfw: initialize and configure
+  // ------------------------------
+  glfwInit();
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+  // glfw window creation
+  // --------------------
+   GLFWwindow *window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Janela Temporizada", NULL,
+                                        NULL);
+
+  if (window == NULL)
+  {
+    std::cout << "Failed to create GLFW window" << std::endl;
+    glfwTerminate();
+    return -1;
+  }
+  glfwMakeContextCurrent(window);
+  glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+  // glad: load all OpenGL function pointers
+  // ---------------------------------------
+  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+  {
+    std::cout << "Failed to initialize GLAD" << std::endl;
+    return -1;
+  }
+
+  //Background color
+  glClearColor(49.0f / 255.0f, 51.0f / 255.0f, 53.0f / 255.0f, 0); //dark grey
+  glClear(GL_COLOR_BUFFER_BIT);
+
+  // render loop
+  // -----------
+  renderLoop(window);
+
+  // glfw: terminate, clearing all previously allocated GLFW resources.
+  // ------------------------------------------------------------------
+  glfwTerminate();
+  return 0;
+}
